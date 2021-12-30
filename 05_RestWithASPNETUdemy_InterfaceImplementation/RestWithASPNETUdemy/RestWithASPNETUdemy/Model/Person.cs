afb6889df6bb0f9a67e78f14a595cd42bcdd4bbc
Gabriel Fonzare <gabrielfonzare@gmail.com>
2021-12-30 11:35:19 -0300

    interface implementation

--- 05_RestWithASPNETUdemy_InterfaceImplementation/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
+++ 05_RestWithASPNETUdemy_InterfaceImplementation/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -7,7 +7,7 @@ namespace RestWithASPNETUdemy.Model
 {
     public class Person
     {
-        public long id { get; set; }
+        public long ID{ get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
