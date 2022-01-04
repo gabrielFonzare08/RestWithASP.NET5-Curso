a8ec6552e570529986ab823ef7fdb3499484d433
Gabriel Fonzare <gabrielfonzare@gmail.com>
2022-01-03 21:40:35 -0300

    Finalizing Implementation

---
+++ 07_RestWithASPNETUdemy_Finalizing Implementation/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Services.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        private IPersonService _personService;
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+
+            if (person == null) return NotFound();
+
+            return Ok(person);
+        }
+
+
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] PersonController person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] PersonController person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+           _personService.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ 07_RestWithASPNETUdemy_Finalizing Implementation/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementation/IPersonService.cs
@@ -0,0 +1,19 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+
+namespace RestWithASPNETUdemy.Services.Implementation
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+        void Delete(long id);
+
+
+    }
+}
